50c82e07fece3bb4622f6ea4de43c15209ddc443
Pedro Menna <pedromenna15@gmail.com>
2026-06-20 14:32:39 -0300

    ajuste nginx

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -12,6 +12,9 @@ resource "aws_lambda_function" "site" {
     log_group  = aws_cloudwatch_log_group.site.name
   }
 
+  # O deploy real da imagem acontece na pipeline SITE DEPLOY via
+  # `aws lambda update-function-code` usando a tag sha-<commit>.
+  # O Terraform nunca deve sobrescrever essa imagem depois de criada.
   lifecycle {
     ignore_changes = [image_uri]
   }
@@ -24,12 +27,23 @@ resource "aws_lambda_function_url" "site" {
   authorization_type = "NONE"
 }
 
+# Permite invocar via Function URL sem autenticação IAM.
 resource "aws_lambda_permission" "url_public" {
   statement_id           = "AllowPublicInvokeFunctionUrl"
-  action                 = "lambda:InvokeFunction" # <- muda aqui
+  action                 = "lambda:InvokeFunctionUrl"
   function_name          = aws_lambda_function.site.function_name
   principal              = "*"
   function_url_auth_type = "NONE"
+
+  depends_on = [aws_lambda_function_url.site]
+}
+
+# Exigido pela AWS além do InvokeFunctionUrl para liberar acesso público.
+resource "aws_lambda_permission" "invoke_public" {
+  statement_id  = "AllowPublicInvokeFunction"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.site.function_name
+  principal     = "*"
 
   depends_on = [aws_lambda_function_url.site]
 }
